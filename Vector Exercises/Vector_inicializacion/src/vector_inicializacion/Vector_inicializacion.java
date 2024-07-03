ece8300933597f63a5479b7722766d91751fa147
Aalbafica <williamlopez12b@gmail.com>
2024-07-03 17:10:45 -0500

    Inicialización del vector
    Programa donde se inicializa un vector, se llena por teclado por parte del usuario y se imprime

---
+++ Vector Exercises/Vector_inicializacion/src/vector_inicializacion/Vector_inicializacion.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package vector_inicializacion;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author willi
+ */
+public class Vector_inicializacion {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // inicializacion
+        int vector[] = new int [4];
+        // Asignacion por teclado
+        Scanner teclado = new Scanner(System.in);
+        // Carga de la matriz
+        for (int i = 0; i < vector.length; i++) {
+            System.out.println("Ingrese el valor para el indice " + i);
+            vector[i] = teclado.nextInt();
+        }
+
+        // Recorrido
+        for (int i = 0; i < vector.length; i++) {
+            System.out.println("Estoy en el indice " + i);
+            System.out.println("Tengo guardado un: " + vector[i]);
+            System.out.println("----------------");
+        }
+    }
+
+}
